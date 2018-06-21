c729e202ed846edb6d812e45c097f290ed2c3416
Limyx826 <limyx0826@gmail.com>
2018-06-21 16:28:58 +0800

    Simple add on

--- G4 Assignment/Form1.vb
+++ G4 Assignment/Form1.vb
@@ -1,7 +1,8 @@
 ﻿'TARUC DEL 2018 Year 1 Semester 1
 'Group 4
 'Members: Lim Yang Xuan, Seow Zhi Hung, Tan Lin Ee, Tan Wei Hang
-'
+'Project: Cinema Booking System
+'Date: 19/06/2018
 
 Public Class Form1
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
@@ -12,5 +13,12 @@ Public Class Form1
     Private Sub btnMembers_Click(sender As Object, e As EventArgs) Handles btnMembers.Click
 
         Label1.Text = "Lim Yang Xuan, Seow Zhi Hung, Tan Lin Ee, Tan Wei Hang"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Form2.Show()
+        Me.Hide()
+
     End Sub
 End Class
