dc76157c811ce892f5a5c7e1a9f3042c872b41ef
Limyx826 <limyx0826@gmail.com>
2018-08-01 16:09:30 +0800

    Add in 01

---
+++ G4 Assignment/frmSeat.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmSeat
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox7_Click(sender As Object, e As EventArgs) Handles PictureBox7.Click
+
+    End Sub
+End Class
