f78112192c1a3752cc85c75f7eedbcce5731a937
Limyx826 <limyx0826@gmail.com>
2018-08-08 17:10:53 +0800

    Revert "????"
    This reverts commit 970f5aef6c6c89f31419885b664a59543d1fa73e.

--- G4 Assignment/frmLogin.vb
+++ G4 Assignment/frmLogin.vb
@@ -1,11 +1,11 @@
-﻿Public Class Form1
+﻿Public Class frmLogin
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblName.Click
 
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         frmHome.Show()
-        Me.Hide()
+        Me.Close()
 
     End Sub
 
@@ -20,9 +20,24 @@
     End Sub
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-        MessageBox.Show("Welcome back, ", "Hi", MessageBoxButtons.OK, MessageBoxIcon.None)
 
-        Me.Hide()
-        frmHome.Show()
+        If txtId.Text.ToUpper() = "ISISKO" And txtPw.Text.ToUpper() = "PNK6035" Then
+            MessageBox.Show("Welcome Back", "Hello", MessageBoxButtons.OK, MessageBoxIcon.None)
+            Me.Hide()
+            frmHome.Show()
+        ElseIf txtId.Text.ToUpper() = "ISISKO" Then
+            MessageBox.Show("Wrong Password", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            txtPw.Focus()
+            txtPw.SelectAll()
+        ElseIf txtPw.Text.ToUpper() = "PNK6035" Then
+            MessageBox.Show("Wrong ID", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            txtId.Focus()
+            txtId.SelectAll()
+        Else
+            MessageBox.Show("Wrong ID And Password", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            txtId.Focus()
+            txtId.SelectAll()
+            Exit Sub
+        End If
     End Sub
 End Class
--- G4 Assignment/frmMRegister.vb
+++ G4 Assignment/frmMRegister.vb
@@ -8,82 +8,77 @@ Public Class frmMRegister
         TxtPass.Copy()
     End Sub
 
-    Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
-        Dim a As Boolean = False
-        If picno1.Visible = True Then
-            a = False
-        End If
-        If picno2.Visible = True Then
-            a = False
-        End If
-        If picno3.Visible = True Then
-            a = False
-        End If
-        If picno5.Visible = True Then
-            a = False
-        End If
-        If picno6.Visible = True Then
-            a = False
-        End If
-        If a = False Then
-            MessageBox.Show("Please fill all empty space !!!")
-        Else
-            MessageBox.Show("Thank you for register !!!")
-            Me.Close()
-            frmHome.Show()
-        End If
-    End Sub
 
-    Private Sub btnCheck_Click_1(sender As Object, e As EventArgs) Handles btnCheck.Click
-        If txtName.Text = "" Then
-            picno1.Visible = True
-            picyes1.Visible = False
-            txtName.Focus()
-        Else
-            picyes1.Visible = True
-            picno1.Visible = False
-        End If
-        If TxtPass.Text = "" Then
-            picno6.Visible = True
-            picyes6.Visible = False
-            TxtPass.Focus()
-        Else
-            picno6.Visible = False
-            picyes6.Visible = True
-        End If
-        If txtID.Text = "" Then
-            picyes5.Visible = False
-            picno5.Visible = True
-            txtID.Focus()
-        Else
-            picyes5.Visible = True
-            picno5.Visible = False
-
-        End If
-        If Not MskIC.MaskFull Then
-            picno3.Visible = True
-            picyes3.Visible = False
-            MskIC.Focus()
-        Else
-            picyes3.Visible = True
-            picno3.Visible = False
-        End If
-        If Not MksBirth.MaskFull Then
-            picno2.Visible = True
-            picyes2.Visible = False
-            MksBirth.Focus()
-        Else
-            picno2.Visible = False
-            picyes2.Visible = True
-        End If
-
-
-    End Sub
 
     Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
         If Asc(e.KeyChar) < 65 Or Asc(e.KeyChar) > 90 _
             And Asc(e.KeyChar) < 97 Or Asc(e.KeyChar) > 122 Then
             e.Handled = True
+            picno1.Visible = True
+            picyes1.Visible = False
+        End If
+    End Sub
+
+    Private Sub btnCheck_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
+        If txtCon.Text.ToUpper() = TxtPass.Text.ToUpper() Then
+            picyes7.Visible = True
+            picno7.Visible = False
+        Else
+            picyes7.Visible = False
+            picno7.Visible = True
+            txtCon.Focus()
+            txtCon.SelectAll()
+
+        End If
+        If TxtPass.TextLength < 7 Then
+            picno6.Visible = True
+            picyes6.Visible = False
+            TxtPass.Focus()
+            TxtPass.SelectAll()
+            lblb.Text = "At Least 7 Charaters"
+        Else
+            picno6.Visible = False
+            picyes6.Visible = True
+            lblb.Text = ""
+        End If
+
+        If txtID.TextLength < 6 Then
+            picyes5.Visible = False
+            picno5.Visible = True
+            txtID.Focus()
+            txtID.SelectAll()
+            lbla.Text = "At Least 6 Charaters"
+
+        Else
+            picyes5.Visible = True
+            picno5.Visible = False
+            lbla.Text = ""
+        End If
+        If Not MskIC.MaskFull Then
+            picno3.Visible = True
+            picyes3.Visible = False
+            MskIC.Focus()
+            MskIC.SelectAll()
+        Else
+            picyes3.Visible = True
+            picno3.Visible = False
+
+        End If
+
+        If Not MksBirth.MaskFull Then
+            picno2.Visible = True
+            picyes2.Visible = False
+            MksBirth.Focus()
+            MksBirth.SelectAll()
+        Else
+            picno2.Visible = False
+            picyes2.Visible = True
+
+        End If
+        If txtName.TextLength < 3 Then
             picno1.Visible = True
             picyes1.Visible = False
             txtName.Focus()
